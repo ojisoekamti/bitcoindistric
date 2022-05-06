3dcca3a1f1a51dbf467ecef0e1da72ee17c24016
Abdul Ghoji Hanggoro <abdulghoji31@gmail.com>
2022-05-06 11:18:14 +0700

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -210,85 +210,26 @@
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 
     <script>
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("https://api1.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+        let seriesData = [];
+        for (let i = 0; i < result.length; i++) {
+            const element = result[i];
+            seruesData.push({
+                x: new Date(element[0]),
+                y: [element[1], element[2], element[3], element[4]]
+            })
+        }
         var options = {
             series: [{
-                data: [{
-                        x: new Date(1538778600000),
-                        y: [6629.81, 6650.5, 6623.04, 6633.33]
-                    },
-                    {
-                        x: new Date(1538780400000),
-                        y: [6632.01, 6643.59, 6620, 6630.11]
-                    },
-                    {
-                        x: new Date(1538782200000),
-                        y: [6630.71, 6648.95, 6623.34, 6635.65]
-                    },
-                    {
-                        x: new Date(1538784000000),
-                        y: [6635.65, 6651, 6629.67, 6638.24]
-                    },
-                    {
-                        x: new Date(1538785800000),
-                        y: [6638.24, 6640, 6620, 6624.47]
-                    },
-                    {
-                        x: new Date(1538787600000),
-                        y: [6624.53, 6636.03, 6621.68, 6624.31]
-                    },
-                    {
-                        x: new Date(1538789400000),
-                        y: [6624.61, 6632.2, 6617, 6626.02]
-                    },
-                    {
-                        x: new Date(1538791200000),
-                        y: [6627, 6627.62, 6584.22, 6603.02]
-                    },
-                    {
-                        x: new Date(1538793000000),
-                        y: [6605, 6608.03, 6598.95, 6604.01]
-                    },
-                    {
-                        x: new Date(1538794800000),
-                        y: [6604.5, 6614.4, 6602.26, 6608.02]
-                    },
-                    {
-                        x: new Date(1538796600000),
-                        y: [6608.02, 6610.68, 6601.99, 6608.91]
-                    },
-                    {
-                        x: new Date(1538798400000),
-                        y: [6608.91, 6618.99, 6608.01, 6612]
-                    },
-                    {
-                        x: new Date(1538800200000),
-                        y: [6612, 6615.13, 6605.09, 6612]
-                    },
-                    {
-                        x: new Date(1538802000000),
-                        y: [6612, 6624.12, 6608.43, 6622.95]
-                    },
-                    {
-                        x: new Date(1538803800000),
-                        y: [6623.91, 6623.91, 6615, 6615.67]
-                    },
-                    {
-                        x: new Date(1538805600000),
-                        y: [6618.69, 6618.74, 6610, 6610.4]
-                    },
-                    {
-                        x: new Date(1538807400000),
-                        y: [6611, 6622.78, 6610.4, 6614.9]
-                    },
-                    {
-                        x: new Date(1538809200000),
-                        y: [6614.9, 6626.2, 6613.33, 6623.45]
-                    },
-                    {
-                        x: new Date(1538811000000),
-                        y: [6623.48, 6627, 6618.38, 6620.35]
-                    }
-                ]
+                data: seriesData
             }],
             chart: {
                 type: 'candlestick',
@@ -376,13 +317,6 @@
     </script>
 
     <script>
-        console.log(moment)
-
-        function getData() {
-
-        }
-
-
         function updateTime() {
             var cTime = moment().zone("-08:00").format('LLLL');
             let get_Date = moment.tz(new Date(), 'Asia/Jakarta');
